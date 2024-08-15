f03fba50112734e379e7843469b92c098b959c9e
rudastick <maciek.buczek2013@gmail.com>
2024-08-15 11:54:02 +0200

    Deleted all cars from CarRunner

--- src/main/java/pl/kurs/app/CarRunner.java
+++ src/main/java/pl/kurs/app/CarRunner.java
@@ -4,11 +4,5 @@ import pl.kurs.models.Car;
 
 public class CarRunner {
     public static void main(String[] args) {
-        Car car1 = new Car("Audi","a3",9999);
-        Car car2 = new Car("bmw","seria 3",25);
-        Car car3 = new Car("Mercedes-Benz","klasa c",666);
-        Car car4 = new Car("Seat","najgorszy",1);
-
-
     }
 }
